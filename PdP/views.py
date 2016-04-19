5eca87d26dd9901cde4e37d184884a41d96450ae
Virgil Niati <niativirgil@gmail.com>
2016-04-19 11:15:06 +0200

    new inscription

--- PdP/views.py
+++ PdP/views.py
@@ -2,21 +2,24 @@ from django.shortcuts import render, redirect
 from django.contrib.auth.models import User
 from django.contrib.auth import authenticate, login
 from models import GreenEq, RedEq, YellowEq, BlueEq
+from forms import RegisterForm
+# from forms import RegisterForm
 # Create your views here.
+
 def homepage(request):
-    if request.method == 'GET':
-        return render(request, "homepage.html")
-    elif request.method == "POST":
-        username = request.POST["identhome"]
-        password = request.POST["identmdp"]
-        user = User.objects.create(username=username,
-                                       password=password)
-        if user is not None:
-            if user.is_active:
-                user.backend = 'django.contrib.auth.backends.ModelBackend'
-                login(request, user)
-        return redirect("/server")
+    if request.method == 'POST':
+        register_form = RegisterForm(request.POST)
+        if register_form.is_valid():
+            identifiant = register_form.cleaned_data["identifiant"]
+            mot_de_passe = register_form.cleaned_data["mot_de_passe"]
+            user = User.objects.create(username=identifiant,
+                                       password=mot_de_passe)
+            return redirect('/server')
+    else:
+        register_form = RegisterForm()
+    return render(request, "homepage.html",
+                          {"form":register_form})
 
 def server(request):
-    user = request.user
-    return render(request, "server.html", {"user":user})
+    # user = request.user
+    return render(request, "server.html")
