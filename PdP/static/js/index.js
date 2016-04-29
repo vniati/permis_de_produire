02e74a80938897b38051b4ccf8438db4cdce9d08
vniati <niativirgil@gmail.com>
2016-04-29 19:09:07 +0200

    fin chat 2

--- PdP/static/js/index.js
+++ PdP/static/js/index.js
@@ -1,6 +1,9 @@
+
 var app = require('express')();
 var http = require('http').Server(app);
 var io = require('socket.io')(http);
+var io2 = require('socket.io')(http);
+var http2 = require('http').Server(app);
 
 app.get('/', function(req, res){
   res.sendfile('/home/virgil/Code/Permis_de_Produire/permis_de_produire/PdP/templates/user.html');
@@ -17,12 +20,30 @@ http.listen(8100, function(){
   console.log('listening on :8100');
 });
 
+io.emit('some event');
 
 
-io.emit('some event', { for: 'everyone' });
-
-io.on('connection', function(socket){
+io2.on('connection', function(socket){
   socket.on('chat message', function(msg){
-    io.emit('chat message', msg);
+    io2.emit('chat message', msg);
+  });
+});
+
+io2.on('connection', function(socket2){
+  console.log('a user connected');
+  socket2.on('disconnect', function(){
+    console.log('user disconnected');
+  });
+});
+
+http2.listen(8101, function(){
+  console.log('listening on :8101');
+});
+
+io2.emit('some event');
+
+io2.on('connection', function(socket2){
+  socket2.on('chat message2', function(msg2){
+    io2.emit('chat message2', msg2);
   });
 });
